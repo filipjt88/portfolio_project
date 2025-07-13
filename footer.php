4ae0daf9b804023b69b4726b30460674041244f7
filipjt88 <filip88ks@gmail.com>
2025-07-13 22:13:28 +0200

    update

---
+++ footer.php
@@ -0,0 +1,34 @@
+   <!-- Footer -->
+    <footer class="footer">
+        <div class="footer-container">
+            <div class="footer-left">
+                <p>&copy; 2025 Filip Jotić. All rights reserved.</p>
+            </div>
+
+            <div class="footer-center">
+                <nav class="footer-nav">
+                    <a href="#home">Home</a>
+                    <a href="#about">About</a>
+                    <a href="#projects">Projects</a>
+                    <a href="#blog">Blog</a>
+                    <a href="#skills">Skills</a>
+                </nav>
+            </div>
+
+            <div class="footer-right">
+                <a href="https://github.com/filipjt88" target="_blank"><i class="fab fa-github"></i></a>
+                <a href="https://www.linkedin.com/in/filipjotić" target="_blank"><i class="fab fa-linkedin"></i></a>
+                <a href="https://mail.google.com/mail/?view=cm&fs=1&to=filipjotic.dev@gmail.com" target="_blank"><i
+                        class="fas fa-envelope"></i></a>
+            </div>
+        </div>
+    </footer>
+    <!-- End of Footer -->
+
+    <!-- Js -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- End of Js -->
+     <?php wp_footer(); ?>
+</body>
+
+</html>
---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+function moja_tema_enqueue_scripts() {
+  wp_enqueue_style('main-style', get_stylesheet_uri());
+  wp_enqueue_script('main-js', get_template_directory_uri() . '/main.js', array(), null, true);
+}
+add_action('wp_enqueue_scripts', 'moja_tema_enqueue_scripts');
+?>
