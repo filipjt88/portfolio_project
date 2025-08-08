77e1e417470013b5b334b5257cb1f5eb94e50808
filipjt88 <filip88ks@gmail.com>
2025-08-08 20:59:32 +0200

    save

--- header.php
+++ header.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,7 +11,6 @@
     <!-- Css -->
     <link rel="stylesheet" href="css/main.css">
 </head>
-
 <body>
 
     <!-- Navbar -->
