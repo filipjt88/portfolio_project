56dd93eb94ae162619532360cf10ba693c91061c
filipjt88 <filip88ks@gmail.com>
2025-08-22 22:39:57 +0200

    footer

--- footer.php
+++ footer.php
@@ -29,7 +29,9 @@
     <!-- Js -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- End of Js -->
+     <!-- Footer -->
      <?php wp_footer(); ?>
+     <!-- End of footer -->
 </body>
 
 </html>
