b2fa372f7e1cd0fc7b9ab66914615c8a85457487
filipjt88 <filip88ks@gmail.com>
2025-08-25 19:04:55 +0200

    save

--- header.php
+++ header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo('name'); ?></title>
+    <!-- Wp head -->
     <?php wp_head(); ?>
     <!-- Bootstrap 5-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
