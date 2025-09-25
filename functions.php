623d07ba14bf882a8afd777c64f3923045517f4c
filipjt88 <filip88ks@gmail.com>
2025-09-25 21:12:57 +0200

    Save

--- functions.php
+++ functions.php
@@ -1,7 +1,9 @@
 <?php
+// Portfolio function
 function portfolio_enqueue_scripts() {
   wp_enqueue_style('main-style', get_stylesheet_uri());
   wp_enqueue_script('main-js', get_template_directory_uri() . '/main.js', array(), null, true);
 }
 add_action('wp_enqueue_scripts', 'portfolio_enqueue_scripts');
+// End of portfolio function
 ?>
